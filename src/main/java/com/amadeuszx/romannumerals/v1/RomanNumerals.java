944596ae26bd1c02f932c95ba1a472b718f4179e
ulwar <olszewski_marcin@wp.pl>
2023-03-10 19:17:26 +0100

    RomanNumerals - refactor p1

--- src/main/java/com/amadeuszx/romannumerals/v1/RomanNumerals.java
+++ src/main/java/com/amadeuszx/romannumerals/v1/RomanNumerals.java
@@ -6,72 +6,98 @@ package com.amadeuszx.romannumerals.v1;
 public class RomanNumerals {
 
     public String convert(int number) {
-        String result = "";
+        Step step = new Step("", number);
 
-        if (number / 1000 < 4) {
-            result += "M".repeat(number / 1000);
-            number = number % 1000;
+        if (step.getNumber() / 1000 < 4) {
+            step.setResult("M".repeat(step.getNumber() / 1000));
+            step.setNumber(1000);
         }
 
-        if (number / 900 == 1) {
-            result += "CM";
-            number = number % 900;
+        if (step.getNumber() / 900 == 1) {
+            step.setResult("CM");
+            step.setNumber(900);
         }
 
-        if (number / 500 == 1) {
-            result += "D";
-            number = number % 500;
+        if (step.getNumber() / 500 == 1) {
+            step.setResult("D");
+            step.setNumber(500);
         }
 
-        if (number / 400 == 1) {
-            result += "CD";
-            number = number % 400;
+        if (step.getNumber() / 400 == 1) {
+            step.setResult("CD");
+            step.setNumber(400);
         }
 
-        if (number / 100 < 4) {
-            result += "C".repeat(number / 100);
-            number = number % 100;
+        if (step.getNumber() / 100 < 4) {
+            step.setResult("C".repeat(step.getNumber() / 100));
+            step.setNumber(100);
         }
 
-        if (number / 90 == 1) {
-            result += "XC";
-            number = number % 90;
+        if (step.getNumber() / 90 == 1) {
+            step.setResult("XC");
+            step.setNumber(90);
         }
 
-        if (number / 50 == 1) {
-            result += "L";
-            number = number % 50;
+        if (step.getNumber() / 50 == 1) {
+            step.setResult("L");
+            step.setNumber(50);
         }
 
-        if (number / 40 == 1) {
-            result += "XL";
-            number = number % 40;
+        if (step.getNumber() / 40 == 1) {
+            step.setResult("XL");
+            step.setNumber(40);
         }
 
-        if (number / 10 < 4) {
-            result += "X".repeat(number / 10);
-            number = number % 10;
+        if (step.getNumber() / 10 < 4) {
+            step.setResult("X".repeat(step.getNumber() / 10));
+            step.setNumber(10);
         }
 
-        if (number / 9 == 1) {
-            result += "IX".repeat(1);
-            number = number % 9;
+        if (step.getNumber() / 9 == 1) {
+            step.setResult("IX".repeat(1));
+            step.setNumber(9);
         }
 
-        if (number / 5 == 1) {
-            result += "V".repeat(1);
-            number = number % 5;
+        if (step.getNumber() / 5 == 1) {
+            step.setResult("V".repeat(1));
+            step.setNumber(5);
         }
 
-        if (number / 4 == 1) {
-            result += "IV".repeat(1);
-            number = number % 4;
+        if (step.getNumber() / 4 == 1) {
+            step.setResult("IV".repeat(1));
+            step.setNumber(4);
         }
 
-        if (number < 4) {
-            result += "I".repeat(number);
+        if (step.getNumber() < 4) {
+            step.setResult("I".repeat(step.getNumber()));
         }
 
-        return result;
+        return step.getResult();
+    }
+
+    private static class Step {
+        private String result;
+        private int number;
+
+        public Step(String result, int number) {
+            this.result = result;
+            this.number = number;
+        }
+
+        public String getResult() {
+            return result;
+        }
+
+        public void setResult(String result) {
+            this.result += result;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public void setNumber(int modulo) {
+            this.number %= modulo;
+        }
     }
 }
