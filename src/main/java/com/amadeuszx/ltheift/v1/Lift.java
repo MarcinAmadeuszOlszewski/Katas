6eab4e0de8e887ddb31543f8249a42ecddbaba9a
ulwar <olszewski_marcin@wp.pl>
2023-03-17 19:45:03 +0100

    The Lift a lift delivers passengers to requested floors

--- src/main/java/com/amadeuszx/ltheift/v1/Lift.java
+++ src/main/java/com/amadeuszx/ltheift/v1/Lift.java
@@ -4,10 +4,20 @@ package com.amadeuszx.ltheift.v1;
  * https://kata-log.rocks/lift-kata
  */
 public class Lift {
-    public String showCurrentFlor() {
-        return "0";
+    private int floor;
+
+    public String showCurrentFloor() {
+        return String.valueOf(floor);
     }
 
     public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public void setDestination(int destination) {
+    }
+
+    public String showMoveInformation() {
+        return null;
     }
 }
--- src/test/java/com/amadeuszx/ltheift/v1/LiftTest.java
+++ src/test/java/com/amadeuszx/ltheift/v1/LiftTest.java
@@ -9,7 +9,7 @@ class LiftTest {
     /**
      * a lift responds to calls containing a source floor and direction
      * !a lift has an attribute floor, which describes it’s current location
-     * a lift delivers passengers to requested floors
+     * !a lift delivers passengers to requested floors
      * ! you may implement current floor monitor
      * you may implement direction arrows
      * you may implement doors (opening and closing)
@@ -21,7 +21,7 @@ class LiftTest {
     @Test
     @DisplayName("current floor monitor")
     void currentFloorMonitor() {
-        final String currentFloor = lift.showCurrentFlor();
+        final String currentFloor = lift.showCurrentFloor();
         assertEquals("0", currentFloor);
     }
 
@@ -29,8 +29,17 @@ class LiftTest {
     @DisplayName("a lift has an attribute floor, which describes it’s current location")
     void liftFloorState() {
         lift.setFloor(5);
-        final String currentFloor = lift.showCurrentFlor();
+        final String currentFloor = lift.showCurrentFloor();
         assertEquals("5", currentFloor);
+    }
+
+    @Test
+    @DisplayName("a lift delivers passengers to requested floors")
+    void deliveryInformation() {
+        lift.setFloor(5);
+        lift.setDestination(3);
+        final String moveInformation = lift.showMoveInformation();
+        assertEquals("2 floors down from 5 to 3", moveInformation);
     }
 
 }
